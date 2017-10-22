93213f7849e6208b17ca7fc9a9089b5b7a93c375
Lucas Sanz Gorostiaga <lucas.sg.07@outlook.com>
2017-10-22 17:27:03 -0300

    Flight constructor, private fields and javadoc added. Airport javadoc corrected.

--- src/Graph/Airport.java
+++ src/Graph/Airport.java
@@ -4,8 +4,10 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
- * The Airport class is an implementation of an airport simulation consisting of the airport's name, its location and a
- * list of the airport's outbound and return flights.
+ * The Airport class is an implementation of an airport consisting of the airport's name, its location and a list of the
+ * airport's outbound and return flights.
+ * The Airport class will be used as a node in a directed graph. The arcs that part from or arrive to a node are set in
+ * the flights list, each flight will consist of an origin and a destination indicating the arc's direction.
  */
 public class Airport {
     /**
@@ -18,7 +20,9 @@ public class Airport {
      */
     private Coordinate position;
     /**
-     * The list of flights that part from (outbound flight) or return to (return flight) the airport.
+     * The list of flights that part from (outbound flight) or return to (return flight) the airport. An outbound flight
+     * will indicate an arc that parts from this airport, which will in turn work as the flight's tail. A return flight
+     * will indicate an arc that arrives to this airport which will in turn work as the flight's head.
      */
     private List<Flight> flights;
 
--- src/Graph/Flight.java
+++ src/Graph/Flight.java
@@ -1,7 +1,66 @@
 package Graph;
 
+import java.sql.Time;
+import java.util.List;
+
 /**
- *
+ * The Flight class is an implementation of a flight consisting of the airplane's airline, the flight's number, a list
+ * of the days on which the flight departs, the departure airport, the arrival airport, the time of departure, the
+ * flight's duration and its pricing. It is necessary to keep in mind that there is no airplane implementation and if
+ * there is any mention of such it is only to help the reader understand the context.
+ * The Flight class will be used as an arc to connect two airports in a directed graph. A flight's 'weight' will be
+ * given by either its price or its duration.
  */
 public class Flight {
+    /**
+     * The name of the airplane's airline.
+     */
+    private String airline;
+    /**
+     * The flight number is a flight's ID under the same airline. There can exist two flights with the same flight
+     * number from different airlines, but there cannot be any two flights with the same flight number under the same
+     * airline.
+     */
+    private int flightNr;
+    /**
+     * The list of days on which the flight departs. The days will be represented by two character strings as such:
+     * "Lu" (lunes), "Ma" (martes), "Mi" (miercoles), "Ju" (jueves), "Vi" (viernes), "Sa" (sabado) and "Do" (domingo).
+     */
+    private List<String> flightDays;
+    /**
+     * The origin or departure airport is the the flight's tail in the directed graph.
+     */
+    private Airport origin;
+    /**
+     * The destination or arrival airport is the flight's head in the directed graph.
+     */
+    private Airport destination;
+    /**
+     * The time at which the flight departs given in 24hs hour:minutes format (HH:MM).
+     */
+    private Time departureTime;
+    /**
+     * The duration of the flight given in the following format [hours]h[minutes]m.
+     */
+    private Time duration;
+    /**
+     * The flight's price is a real number equal or bigger than 0 with at most two decimals.
+     */
+    private double price;
+
+
+    public Flight(String airline, int flightNr, List<String> flightDays, Airport origin, Airport destination,
+                  Time departureTime, Time duration, double price) {
+        this.airline = airline;
+        this.flightNr = flightNr;
+        this.flightDays = flightDays;
+        this.origin = origin;
+        this.destination = destination;
+        this.departureTime = departureTime;
+        this.duration = duration;
+        // The product and division by 10^2 is made to round up to two decimal places.
+        this.price = price * 100 / 100;
+    }
+
+    
 }
