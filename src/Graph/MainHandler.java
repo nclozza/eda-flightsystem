c3e643d7b2c96ed152784139614b496bfa025bc4
BRitorto97 <britorto@itba.edu.ar>
2017-11-03 13:49:33 -0300

    Added some changes

--- src/Graph/MainHandler.java
+++ src/Graph/MainHandler.java
@@ -14,7 +14,7 @@ public class MainHandler {
     private static final String EXIT_COMMAND = "exit";
     private FlightSystem flightSystem;
 
-    public MainHandler(){
+    public MainHandler() {
         flightSystem = new FlightSystem();
     }
 
@@ -25,153 +25,162 @@ public class MainHandler {
     public void runCode() {
         String input = null;
 
-            while (true) {
-                System.out.println("Enter some text, or '" + EXIT_COMMAND + "' to quit");
-                System.out.print("> ");
+        while (true) {
+            System.out.println("Enter some text, or '" + EXIT_COMMAND + "' to quit");
+            System.out.print("> ");
 
-                try {
-                    input = ConsoleReader.readingFromConsole();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                String[] aux = input.split(" ");
-                switch (aux[0]){
-                    case "insert":
-                        if(aux.length>=2){
-                            switch (aux[1]){
-                                case "airport":
-                                    if ( (aux.length != 5)) System.out.println("Wrong input");
-                                    else{
-                                        ValidateData.validateName(aux[2]); //aux[2] is name
-                                        ValidateData.validateLat(aux[3]); //aux[3] is lat
-                                        ValidateData.validateLon(aux[4]); //aux[4] is long
+            try {
+                input = ConsoleReader.readingFromConsole();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            String[] aux = input.split(" ");
+            switch (aux[0]) {
+                case "insert":
+                    if (aux.length >= 2) {
+                        switch (aux[1]) {
+                            case "airport":
+                                if ((aux.length != 5)) System.out.println("Wrong input");
+                                else {
+                                    ValidateData.validateName(aux[2]); //aux[2] is name
+                                    ValidateData.validateLat(aux[3]); //aux[3] is lat
+                                    ValidateData.validateLon(aux[4]); //aux[4] is long
 
-                                        Airport auxAirport = new Airport(aux[2], Integer.parseInt(aux[3]),
-                                                Integer.parseInt(aux[4]));
+                                    Airport auxAirport = new Airport(aux[2], Integer.parseInt(aux[3]),
+                                            Integer.parseInt(aux[4]));
 
-                                        if (flightSystem.getAirports().contains(auxAirport)){
-                                            System.out.println("Airport already exists");
-                                        }
-                                        else {
-                                            System.out.println("Inserted airport " + aux[2]);
-                                            flightSystem.addAirport(auxAirport);
-                                        }
+                                    if (flightSystem.getAirports().contains(auxAirport)) {
+                                        System.out.println("Airport already exists");
+                                    } else {
+                                        System.out.println("Inserted airport " + aux[2]);
+                                        flightSystem.addAirport(auxAirport);
                                     }
-                                    break;
-                                case "all":
-                                    if(aux.length>=3){
-                                        if (aux[2].equals("airports")) {
-                                            if (aux.length == 5){
-                                                String path = aux[3];
-                                                processingFile(path, aux[4]); //aux[4] is append or replace
-                                                System.out.println(aux[4].toUpperCase()+" all airports");
-                                            } else
-                                                System.out.println("Wrong input");
-                                        } else if (aux[2].equals("flight")) {
-                                            if (aux.length == 5) {
-                                                String path = aux[3];
-                                                processingFile(path, aux[4]); //aux[4] is append or replace
-                                                System.out.println(aux[4].toUpperCase()+" all flights");
-                                            } else
-                                                System.out.println("Wrong input");
-                                        }
+                                }
+                                break;
+                            case "all":
+                                if (aux.length >= 3) {
+                                    if (aux[2].equals("airports")) {
+                                        if (aux.length == 5) {
+                                            String path = aux[3];
+                                            processingFile(path, aux[4]); //aux[4] is append or replace
+                                            System.out.println(aux[4].toUpperCase() + " all airports");
+                                        } else
+                                            System.out.println("Wrong input");
+                                    } else if (aux[2].equals("flight")) {
+                                        if (aux.length == 5) {
+                                            String path = aux[3];
+                                            processingFile(path, aux[4]); //aux[4] is append or replace
+                                            System.out.println(aux[4].toUpperCase() + " all flights");
+                                        } else
+                                            System.out.println("Wrong input");
                                     }
-                                    break;
-                                case "flight":
-                                    if ( !(aux.length == 10)) System.out.println("Wrong input");
-                                    else{
-                                        System.out.println("Inserting flight " + aux[2] + " - " + aux[3]);
+                                }
+                                break;
+                            case "flight":
+                                if (!(aux.length == 10)) System.out.println("Wrong input");
+                                else {
+                                    System.out.println("Inserting flight " + aux[2] + " - " + aux[3]);
+                                    //**insert flight (aerolinea) (nroVuelo) (diasSemana) (origen) (destino)
+                                    // (horaSalida) (duracion) (precio)** *ver comentario para detalles*
+                                    boolean state1 = ValidateData.validateName(aux[2]) && ValidateData.validateFlightNumber(aux[3]);
+                                    boolean state2 = ValidateData.validateDay(aux[4]) && ValidateData.validateOrigin(aux[5]);
+                                    boolean state3 = ValidateData.validateDestiny(aux[6]) && ValidateData.validateTime(aux[7]);
+                                    boolean state4 = ValidateData.validateLong(aux[8]) && ValidateData.validatePrice(aux[9]);
+                                    if (!(state1 && state2 && state3 && state4))
+                                        System.out.println("Wrong input");
+                                    else {
+                                        //Call insert
                                     }
-                                    break;
-                                default:
+                                }
+                                break;
+                            default:
+                                System.out.println("Wrong input");
+                        }
+                    } else {
+                        System.out.println("Wrong input");
+                    }
+                    break;
+                case "delete":
+                    if (aux.length >= 2) {
+                        switch (aux[1]) {
+                            case "airport":
+                                if (!(aux.length == 3)) System.out.println("Wrong input");
+                                else {
+                                    if (flightSystem.deleteAirport(aux[2]))
+                                        System.out.println("Deleting airport " + aux[2]);
+                                    else
+                                        System.out.println("Airport " + aux[2] + " does not exist");
+                                }
+                                break;
+                            case "flight":
+                                if (!(aux.length == 4)) System.out.println("Wrong input");
+                                else if(!(ValidateData.validateName(aux[2]) && ValidateData.validateFlightNumber(aux[3]))) {
+                                System.out.println("Deleting flight " + aux[2] + " - " + aux[3]);
+                            } else{
+                                System.out.println("The flight you want to delete does not exist");
+                            }
+
+                        break;
+                        case "all":
+                            if (!(aux.length == 3)) System.out.println("Wrong input");
+                            else {
+
+                                if (aux[2].equals("airport")) {
+                                    System.out.println("Deleting all airports");
+                                    flightSystem.deleteAllAirports();
+                                } else if (aux[2].equals("flight")) {
+                                    System.out.println("Deleting all flights");
+                                    flightSystem.deleteAllFlights();
+                                } else
                                     System.out.println("Wrong input");
                             }
-                        }
-                        else {
+                            break;
+                        default:
                             System.out.println("Wrong input");
-                        }
-                        break;
-                    case "delete":
-                         if(aux.length>=2){
-                             switch (aux[1]){
-                                 case "airport":
-                                     if (!(aux.length == 3)) System.out.println("Wrong input");
-                                     else{
-                                         if (flightSystem.deleteAirport(aux[2]))
-                                            System.out.println("Deleting airport "+ aux[2]);
-                                         else
-                                             System.out.println("Airport " + aux[2] + " does not exist");
-                                     }
-                                     break;
-                                 case "flight":
-                                     if (!(aux.length == 4)) System.out.println("Wrong input");
-                                     else{
-                                         System.out.println("Deleting flight "+aux[2]+ " - " + aux[3]);
+                            break;
+                    }
+            }
+                         else{
+                System.out.println("Wrong input");
+            }
+            break;
+            case "findRoute":
+                System.out.println(aux[0]);
+                //Sintaxis: findRoute [origen] [destino] [prioridad{ft|pr|tt}] [diasSemana]
+                //Aca solo falta validar o ver como haces para validar lo de prioridad
+            case "worldTrip":
+                System.out.println(aux[0]);
+                //Sintaxis: worldTrip [origen] [prioridad{ft|pr|tt}] [diasSemana]
+                //Aca solo falta validar o ver como haces para validar lo de prioridad
+            case "exit":
+                System.out.print("See you soon");
+                return;
+            case "outputFormat":
+                if (aux.length != 3) {
+                    System.out.println("Wrong input");
+                } else {
+                    if (aux[1].equals("text")) {
+                        //Do something
+                    } else if (aux[1].equals("KML")) {
+                        //Do something
+                    } else
+                        System.out.println("Wrong input");
 
-
-                                     }
-                                     break;
-                                 case "all":
-                                     if (!(aux.length == 3)) System.out.println("Wrong input");
-                                     else{
-
-                                         if (aux[2].equals("airport")) {
-                                             System.out.println("Deleting all airports");
-                                             flightSystem.deleteAllAirports();
-                                         } else if (aux[2].equals("flight")) {
-                                             System.out.println("Deleting all flights");
-                                             flightSystem.deleteAllFlights();
-                                         } else
-                                             System.out.println("Wrong input");
-                                     }
-                                     break;
-                                 default:
-                                     System.out.println("Wrong input");
-                                     break;
-                             }
-                         }
-                         else {
-                             System.out.println("Wrong input");
-                         }
-                         break;
-                    case "findRoute":
-                        System.out.println(aux[0]);
-                        //Sintaxis: findRoute [origen] [destino] [prioridad{ft|pr|tt}] [diasSemana]
-                        //Aca solo falta validar o ver como haces para validar lo de prioridad
-                    case "worldTrip":
-                        System.out.println(aux[0]);
-                        //Sintaxis: worldTrip [origen] [prioridad{ft|pr|tt}] [diasSemana]
-                        //Aca solo falta validar o ver como haces para validar lo de prioridad
-                    case "exit":
-                        System.out.print("See you soon");
-                        return;
-                    case "outputFormat":
-                        if (aux.length != 3){
-                            System.out.println("Wrong input");
-                        }
-                        else{
-                            if (aux[1].equals("text")) {
-                                //Do something
-                            } else if (aux[1].equals("KML")) {
-                                //Do something
-                            }else
-                                System.out.println("Wrong input");
-
-                            if (aux[2].equals("archivo")) {
-                                //Do something
-                            } else if (aux[2].equals("stdout")) {
-                                //Do something
-                            } else
-                                System.out.println("Wrong input");
-                        }
-                        break;
-                    default:
+                    if (aux[2].equals("archivo")) {
+                        //Do something
+                    } else if (aux[2].equals("stdout")) {
+                        //Do something
+                    } else
                         System.out.println("Wrong input");
                 }
-
-            }
+                break;
+            default:
+                System.out.println("Wrong input");
+        }
 
     }
+
+}
 
     private void processingFile(String path, String option){
         try {
